8ac1b626083fc17ddadf8c5b4b4996878d8560d1
Retky <jose.arredondo.a@outlook.com>
2022-06-07 16:01:05 -0500

    Add 'invoice_items & treatments' tables
    With FK & link references

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -27,3 +27,29 @@ CREATE TABLE invoices(
 
 ALTER table medical_histories drop column patient_id;
 ALTER TABLE medical_histories add column patient_id int references patients(id);
+
+-- Create table named invoice_items
+CREATE TABLE invoice_items(
+    id int primary key,
+    unit_price decimal,
+    quantity int,
+    total_price decimal,
+    invoice_id int,
+    treatment_id int,
+    CONSTRAINT fkey_invoice_id FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+);
+
+--  Create table named treatments
+CREATE TABLE treatments(
+    id int NOT NULL,
+    type varchar(255),
+    name varchar(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY(id) REFERENCES medical_histories(id) ON UPDATE CASCADE
+);
+
+BEGIN;
+ALTER TABLE invoice_items
+    DROP COLUMN treatment_id,
+    ADD COLUMN treatment_id int REFERENCES treatments(id);
+COMMIT;
